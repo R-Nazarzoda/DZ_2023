8a9a93c2bf9310e081a136e4bf71799b69ff519a
DEZhelnin <91614275+DEZhelnin@users.noreply.github.com>
2023-05-31 21:32:01 +0300

    Create Form1.Designer.cs

---
+++ shariki/2kL-2023-02-09-AnimDblBfr/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace _2kL_2023_02_09_AnimDblBfr
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mainPanel = new Panel();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            mainPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            mainPanel.BackColor = Color.White;
+            mainPanel.Location = new Point(14, 16);
+            mainPanel.Margin = new Padding(3, 4, 3, 4);
+            mainPanel.Name = "mainPanel";
+            mainPanel.Size = new Size(945, 490);
+            mainPanel.TabIndex = 0;
+            mainPanel.MouseClick += mainPanel_MouseClick;
+            mainPanel.Resize += mainPanel_Resize;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.Location = new Point(817, 647);
+            button2.Name = "button2";
+            button2.Size = new Size(142, 29);
+            button2.TabIndex = 0;
+            button2.Text = "обновить счёт";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(270, 545);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(426, 131);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(972, 688);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(mainPanel);
+            Margin = new Padding(3, 4, 3, 4);
+            MinimumSize = new Size(455, 451);
+            Name = "Form1";
+            Text = "Анимация";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel mainPanel;
+        private Button button2;
+        private TextBox textBox1;
+    }
+}
